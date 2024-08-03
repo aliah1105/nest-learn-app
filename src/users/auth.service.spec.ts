65e549a9aad4465388b5ff44b6f04fc3232ca707
aliah1105 <aliahadi.f1105@gmail.com>
2024-08-03 20:16:47 +0330

    write authService unit tests

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+it('can create an instance of AuthService', async () => {
+
+    const fakeUserService: Partial<UsersService> = {
+        find: () => Promise.resolve([]),
+        create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
+    };
+
+    const module = await Test.createTestingModule({
+        providers: [
+            AuthService,
+            {
+                provide: UsersService,
+                useValue: fakeUserService
+            }
+        ]
+    }).compile();
+
+    const service = module.get(AuthService);
+
+    expect(service).toBeDefined();
+});
