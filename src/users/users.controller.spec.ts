f692538ba2d579e85f8c73f790b31caa109f6433
aliah1105 <aliahadi.f1105@gmail.com>
2024-08-13 20:18:53 +0330

    add user controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      findOne: () => { },
+      find: () => { },
+      update: () => { },
+      remove: () => { },
+    };
+
+    fakeAuthService = {
+      signup: () => { },
+      signin: () => { }
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
