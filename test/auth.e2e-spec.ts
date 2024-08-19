cc478cbb798ef5a4ed4183e7896480bdb496835c
aliah1105 <aliahadi.f1105@gmail.com>
2024-08-19 21:52:20 +0330

    add e2e tests for Auth system

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Auth System (e2e) tests', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('test signup working', async () => {
+    const email = 'askj@gmail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'test1234' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
