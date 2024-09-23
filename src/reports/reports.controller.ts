532895e5514717ee143c1b5539efec001bca9df3
aliah1105 <aliahadi.f1105@gmail.com>
2024-09-23 21:50:18 +0330

    add relation with typeorm in report and user

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,15 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGaurd } from '../gaurds/auth.guard';
+import { CurrentUser } from '../users/decorators/currentUser.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportService: ReportsService) {}
     @Post()
     @UseGuards(AuthGaurd)
-    createReport(@Body() body: CreateReportDto) { 
-        return this.reportService.create(body);
+    @Serialize(ReportDto)
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) { 
+        return this.reportService.create(body, user);
     }
 }
