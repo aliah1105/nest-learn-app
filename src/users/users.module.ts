4b9f27b9de01ae6ef53072df0d7d7243f0437d2a
aliah1105 <aliahadi.f1105@gmail.com>
2024-10-12 19:44:21 +0330

    create admin guard for approve a report

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
@@ -19,4 +20,4 @@ import { CurrentUserInterceptor } from './interceptors/currentUser.interceptor';
     },
   ],
 })
-export class UsersModule {}
+export class UsersModule { }
