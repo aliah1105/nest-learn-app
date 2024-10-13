0a8dcc9d7717a155cc86bc183b0eb686a2f3e4cf
aliah1105 <aliahadi.f1105@gmail.com>
2024-10-13 20:45:55 +0330

    write get estimate handler and dto

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Controller, Post, Body, UseGuards, Patch, Param } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Patch, Param, Get, Query } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
@@ -10,19 +10,25 @@ import { CurrentUser } from '../users/decorators/currentUser.decorator';
 import { User } from 'src/users/user.entity';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/ApproveReportDto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
-    constructor(private reportService: ReportsService) {}
+    constructor(private reportService: ReportsService) { }
     @Post()
     @UseGuards(AuthGaurd)
     @Serialize(ReportDto)
-    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) { 
+    createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
         return this.reportService.create(body, user);
     }
 
     @Patch('/:id')
     approveReport(@Body() body: ApproveReportDto, @Param('id') id: string) {
         return this.reportService.changeApproval(+id, body.approved);
+    }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+
     }
 }
